e897069b1f755c4f30bcb56cedc3ecf6ea661522
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-03-06 11:21:21 -0800

    Initial app setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header title="Zillow Rent Tracking" className="App-header">
+        Zillow Rent Tracking
+      </header>
+    </div>
+  );
+}
+
+export default App;
