9a2873070b6e5ad626c95f14543e4494d4486c86
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-03-17 20:28:15 -0700

    Returning zpid address img and coordinates

--- api/main.py
+++ api/main.py
@@ -1,17 +1,18 @@
 # from crypt import methods
-from dataclasses import dataclass
+# from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request
+
 # from flask_cors import CORS
 # from mongo_client import mongo_client
 
 load_dotenv(dotenv_path="./.env.local")
 
 RAPID_API_KEY = os.environ.get("RAPID_API_KEY", default="")
-PROPERTY_EXTENDED_URL = 'https://zillow-com1.p.rapidapi.com/propertyExtendedSearch'
-INDIVIDUAL_PROPERTY_URL = 'https://zillow-com1.p.rapidapi.com/property'
+PROPERTY_EXTENDED_URL = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
+INDIVIDUAL_PROPERTY_URL = "https://zillow-com1.p.rapidapi.com/property"
 
 if not RAPID_API_KEY:
     raise EnvironmentError(
@@ -20,18 +21,30 @@ if not RAPID_API_KEY:
 
 app = Flask(__name__)
 
+
 @app.route("/new-results")
 def new_results():
     parameters = request.args
-    # querystring = {"location":"folsom, ca","status_type":"ForRent","home_type":"Houses"}
+    # querystring={"location":"folsom, ca","status_type":"ForRent","home_type":"Houses"}
     headers = {
-    'x-rapidapi-host': "zillow-com1.p.rapidapi.com",
-    'x-rapidapi-key': RAPID_API_KEY
+        "x-rapidapi-host": "zillow-com1.p.rapidapi.com",
+        "x-rapidapi-key": RAPID_API_KEY,
     }
-    response = requests.request("GET", PROPERTY_EXTENDED_URL, headers=headers, params=parameters)
+    response = requests.request(
+        "GET", PROPERTY_EXTENDED_URL, headers=headers, params=parameters
+    )
 
     data = response.json()
-    return data
+    houses_for_rent = {}
+    for house in data["props"]:
+        houses_for_rent[house["zpid"]] = {
+            "zpid": int(house["zpid"]),
+            "address": house["address"],
+            "image": house["imgSrc"],
+            "longitude": house["longitude"],
+            "latitude": house["latitude"],
+        }
+    return houses_for_rent
 
 
 if __name__ == "__main__":
