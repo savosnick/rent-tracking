43d73812939d6ce73773c734c5d1479d6e9e870c
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-03-30 20:06:31 -0700

    Creaed router for tracked houses tab

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import Tracked from "./Tracked";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/tracked" element={<Tracked />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
