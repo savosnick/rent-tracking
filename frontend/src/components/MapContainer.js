52ddfa4989aac2409a1809c2508304d26060d1d7
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-04-02 20:45:34 -0700

    adjust map size to fit all tracked houses

--- frontend/src/components/MapContainer.js
+++ frontend/src/components/MapContainer.js
@@ -6,7 +6,7 @@ const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API;
 
 const MapContainer = ({ trackedHouses }) => {
   const mapStyles = {
-    height: "100vh",
+    height: "60vh",
     width: "100%",
   };
 
@@ -15,32 +15,108 @@ const MapContainer = ({ trackedHouses }) => {
     location: { lat: house.latitude, lng: house.longitude },
   }));
 
-  const averageLat = (locations) => {
-    let total = 0;
+  const midLat = (locations) => {
+    let min = 1000;
+    let max = -1000;
     for (const key in locations) {
-      total += locations[key]["location"]["lat"];
+      if (locations[key]["location"]["lat"] < min) {
+        min = locations[key]["location"]["lat"];
+      }
+      if (locations[key]["location"]["lat"] > max) {
+        max = locations[key]["location"]["lat"];
+      }
     }
-    console.log(total / locations.length);
-    return total / locations.length;
+    console.log((max + min) / 2);
+    return (max + min) / 2;
   };
 
-  const averageLong = (locations) => {
-    let total = 0;
+  const midLng = (locations) => {
+    let min = 1000;
+    let max = -1000;
     for (const key in locations) {
-      total += locations[key]["location"]["lng"];
+      if (locations[key]["location"]["lng"] < min) {
+        min = locations[key]["location"]["lng"];
+      }
+      if (locations[key]["location"]["lng"] > max) {
+        max = locations[key]["location"]["lng"];
+      }
     }
-    console.log(total / locations.length);
-    return total / locations.length;
+    console.log((max + min) / 2);
+    return (max + min) / 2;
   };
 
   const defaultCenter = {
-    lat: averageLat(locations),
-    lng: averageLong(locations),
+    lat: midLat(locations),
+    lng: midLng(locations),
+  };
+
+  const latRange = (locations) => {
+    let min = 1000;
+    let max = -1000;
+    for (const key in locations) {
+      if (locations[key]["location"]["lat"] < min) {
+        min = locations[key]["location"]["lat"];
+      }
+      if (locations[key]["location"]["lat"] > max) {
+        max = locations[key]["location"]["lat"];
+      }
+    }
+    return max - min;
+  };
+
+  const lngRange = (locations) => {
+    let min = 1000;
+    let max = -1000;
+    for (const key in locations) {
+      if (locations[key]["location"]["lng"] < min) {
+        min = locations[key]["location"]["lng"];
+      }
+      if (locations[key]["location"]["lng"] > max) {
+        max = locations[key]["location"]["lng"];
+      }
+    }
+    return max - min;
+  };
+
+  const zoomLevel = (locations) => {
+    const zoomRange = Math.max(lngRange(locations), latRange(locations));
+    console.log(zoomRange);
+    if (zoomRange < 0.1) {
+      return 12;
+    } else if (zoomRange < 0.2) {
+      return 11;
+    } else if (zoomRange < 0.4) {
+      return 10;
+    } else if (zoomRange < 0.7) {
+      return 9;
+    } else if (zoomRange < 1) {
+      return 8;
+    } else if (zoomRange < 3) {
+      return 7;
+    } else if (zoomRange < 5) {
+      return 6;
+    } else if (zoomRange < 10) {
+      return 6;
+    } else if (zoomRange < 25) {
+      return 5;
+    } else if (zoomRange < 40) {
+      return 4;
+    } else if (zoomRange < 50) {
+      return 4;
+    } else if (zoomRange < 70) {
+      return 2;
+    } else {
+      return 1;
+    }
   };
 
   return (
     <LoadScript googleMapsApiKey={API_KEY}>
-      <GoogleMap mapContainerStyle={mapStyles} zoom={11} center={defaultCenter}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={zoomLevel(locations)}
+        center={defaultCenter}
+      >
         {locations.map((item) => {
           return <Marker key={item.name} position={item.location} />;
         })}
